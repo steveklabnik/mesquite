6a61a308bcea315f1e4fa522c02bcce153567994
OGINO Masanori <masanori.ogino@gmail.com>
2014-07-26 09:17:57 +0900

    Initial import.
    Signed-off-by: OGINO Masanori <masanori.ogino@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mesquite: A Rust Implementation of the MQTT Protocol
+
+## What Is This?
+
+Mesquite is an implementation of the Message Queuing Telemetry Transport (MQTT)
+protocol. It is written in Rust and provided as a library. The target protocol
+version is 3.1.1.
+
+## Current Status
+
+Pre-alpha: Nothing is implemented.
+
+## License
+
+Mesquite is dual-licensed under the MIT License and the Apache License 2.0.
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright (c) 2014 Masanori Ogino
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! # Mesquite: An Implementation of the MQTT Protocol
+
+#![experimental]
+#![crate_name = "mesquite"]
+#![license = "MIT/ASL2"]
