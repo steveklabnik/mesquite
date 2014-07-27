e74c671879a0cfc53e068f425126ecc31d75a9e5
OGINO Masanori <masanori.ogino@gmail.com>
2014-07-27 21:27:15 +0900

    Add constants for control packet types.
    Also, micro-benchmarking tests are now running on Travis CI.
    
    Signed-off-by: OGINO Masanori <masanori.ogino@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,20 @@
 #![experimental]
 #![crate_name = "mesquite"]
 #![license = "MIT/ASL2"]
+#![deny(missing_doc)]
+#![allow(dead_code)]
+
+static CONNECT:     u8 = 0x10;
+static CONNACK:     u8 = 0x20;
+static PUBLISH:     u8 = 0x30;
+static PUBACK:      u8 = 0x40;
+static PUBREC:      u8 = 0x50;
+static PUBREL:      u8 = 0x60;
+static PUBCOMP:     u8 = 0x70;
+static SUBSCRIBE:   u8 = 0x80;
+static SUBACK:      u8 = 0x90;
+static UNSUBSCRIBE: u8 = 0xa0;
+static UNSUBACK:    u8 = 0xb0;
+static PINGREQ:     u8 = 0xc0;
+static PINGRESP:    u8 = 0xd0;
+static DISCONNECT:  u8 = 0xe0;
