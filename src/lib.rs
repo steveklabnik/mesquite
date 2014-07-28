88b2ae4897871f8aadc2e1b98bf3688c4979af5d
OGINO Masanori <masanori.ogino@gmail.com>
2014-07-28 17:40:20 +0900

    Define control packet types as an enum.
    Signed-off-by: OGINO Masanori <masanori.ogino@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -14,17 +14,61 @@
 #![deny(missing_doc)]
 #![allow(dead_code)]
 
-static CONNECT:     u8 = 0x10;
-static CONNACK:     u8 = 0x20;
-static PUBLISH:     u8 = 0x30;
-static PUBACK:      u8 = 0x40;
-static PUBREC:      u8 = 0x50;
-static PUBREL:      u8 = 0x60;
-static PUBCOMP:     u8 = 0x70;
-static SUBSCRIBE:   u8 = 0x80;
-static SUBACK:      u8 = 0x90;
-static UNSUBSCRIBE: u8 = 0xa0;
-static UNSUBACK:    u8 = 0xb0;
-static PINGREQ:     u8 = 0xc0;
-static PINGRESP:    u8 = 0xd0;
-static DISCONNECT:  u8 = 0xe0;
+enum PacketKind {
+    Connect,
+    ConnAck,
+    Publish,
+    PubAck,
+    PubRec,
+    PubRel,
+    PubComp,
+    Subscribe,
+    SubAck,
+    Unsubscribe,
+    UnsubAck,
+    PingReq,
+    PingResp,
+    Disconnect,
+    Reserved
+}
+
+fn to_packet_kind(b: u8) -> PacketKind {
+    match b | 0xf0 {
+        0x10 => Connect,
+        0x20 => ConnAck,
+        0x30 => Publish,
+        0x40 => PubAck,
+        0x50 => PubRec,
+        0x60 => PubRel,
+        0x70 => PubComp,
+        0x80 => Subscribe,
+        0x90 => SubAck,
+        0xa0 => Unsubscribe,
+        0xb0 => UnsubAck,
+        0xc0 => PingReq,
+        0xd0 => PingResp,
+        0xe0 => Disconnect,
+        _ => Reserved
+    }
+}
+
+fn from_packet_kind(k: PacketKind) -> Option<u8> {
+    let b = match k {
+        Connect => 0x10,
+        ConnAck => 0x20,
+        Publish => 0x30,
+        PubAck => 0x40,
+        PubRec => 0x50,
+        PubRel => 0x60,
+        PubComp => 0x70,
+        Subscribe => 0x80,
+        SubAck => 0x90,
+        Unsubscribe => 0xa0,
+        UnsubAck => 0xb0,
+        PingReq => 0xc0,
+        PingResp => 0xd0,
+        Disconnect => 0xe0,
+        _ => return None
+    };
+    Some(b)
+}
