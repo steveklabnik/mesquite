2bc2e95305e817ad323e5b2f07a464039fd5ec9f
OGINO Masanori <masanori.ogino@gmail.com>
2014-07-29 01:06:30 +0900

    Add more tests and fix a bug in from_code.
    Signed-off-by: OGINO Masanori <masanori.ogino@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@ pub enum PacketKind {
 impl PacketKind {
     /// Returns a `PacketKind` value corresponding to `code`.
     pub fn from_code(code: u8) -> PacketKind {
-        match code | 0xf0 {
+        match code & 0xf0 {
             0x10 => Connect,
             0x20 => ConnAck,
             0x30 => Publish,
@@ -99,13 +99,57 @@ mod test {
     use super::*;
 
     #[test]
+    fn from_code() {
+        // Without flag bits
+        assert_eq!(PacketKind::from_code(0x10), Connect);
+        assert_eq!(PacketKind::from_code(0x20), ConnAck);
+        assert_eq!(PacketKind::from_code(0x30), Publish);
+        assert_eq!(PacketKind::from_code(0x40), PubAck);
+        assert_eq!(PacketKind::from_code(0x50), PubRec);
+        assert_eq!(PacketKind::from_code(0x60), PubRel);
+        assert_eq!(PacketKind::from_code(0x70), PubComp);
+        assert_eq!(PacketKind::from_code(0x80), Subscribe);
+        assert_eq!(PacketKind::from_code(0x90), SubAck);
+        assert_eq!(PacketKind::from_code(0xa0), Unsubscribe);
+        assert_eq!(PacketKind::from_code(0xb0), UnsubAck);
+        assert_eq!(PacketKind::from_code(0xc0), PingReq);
+        assert_eq!(PacketKind::from_code(0xd0), PingResp);
+        assert_eq!(PacketKind::from_code(0xe0), Disconnect);
+
+        // With flag bits
+        assert_eq!(PacketKind::from_code(0x3f), Publish);
+        assert_eq!(PacketKind::from_code(0x62), PubRel);
+        assert_eq!(PacketKind::from_code(0x82), Subscribe);
+        assert_eq!(PacketKind::from_code(0xa2), Unsubscribe);
+    }
+
+    #[test]
     fn from_code_reserved() {
+        // Without flag bits
         assert_eq!(PacketKind::from_code(0x00), Reserved);
         assert_eq!(PacketKind::from_code(0xf0), Reserved);
 
         // With flag bits
         assert_eq!(PacketKind::from_code(0x01), Reserved);
         assert_eq!(PacketKind::from_code(0xff), Reserved);
+    }
+
+    #[test]
+    fn to_code() {
+        assert_eq!(PacketKind::to_code(Connect), Some(0x10));
+        assert_eq!(PacketKind::to_code(ConnAck), Some(0x20));
+        assert_eq!(PacketKind::to_code(Publish), Some(0x30));
+        assert_eq!(PacketKind::to_code(PubAck), Some(0x40));
+        assert_eq!(PacketKind::to_code(PubRec), Some(0x50));
+        assert_eq!(PacketKind::to_code(PubRel), Some(0x60));
+        assert_eq!(PacketKind::to_code(PubComp), Some(0x70));
+        assert_eq!(PacketKind::to_code(Subscribe), Some(0x80));
+        assert_eq!(PacketKind::to_code(SubAck), Some(0x90));
+        assert_eq!(PacketKind::to_code(Unsubscribe), Some(0xa0));
+        assert_eq!(PacketKind::to_code(UnsubAck), Some(0xb0));
+        assert_eq!(PacketKind::to_code(PingReq), Some(0xc0));
+        assert_eq!(PacketKind::to_code(PingResp), Some(0xd0));
+        assert_eq!(PacketKind::to_code(Disconnect), Some(0xe0));
     }
 
     #[test]
